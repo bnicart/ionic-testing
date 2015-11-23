39a504a17a8fd72efae15741b019a1e1223c8ada
Billy Nicart <bnicart@you-source.com>
2015-11-23 17:00:31 +0800

    [Ionic Tabs and Sample test files]

---
+++ tests/Controllers/controllers.tests.js
@@ -0,0 +1,14 @@
+describe('Controllers', function() {
+  var scope;
+
+  beforeEach(module('starter.controllers'));
+
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    $controller('AccountCtrl', {$scope: scope});
+  }));
+
+  it('should have enabled friends to be true', function() {
+    expect(scope.settings.enableFriends).toEqual(true);
+  });
+});
